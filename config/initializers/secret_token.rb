badb86a411516b0ca1bb5b4a5714959ac527ba1d
Max Rogers <maxrogers90@gmail.com>
2014-03-05 17:54:56 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MADAPP::Application.config.secret_key_base = '5a2a4a2fb72553d0193ba4cbecd2322825df88063fd53b66dc2f94a5a058cfe8106a67ccbe5220c2e25d4c7fb6d08dfe79c56dd1f702e5881ce121221fb60602'
